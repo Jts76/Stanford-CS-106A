1106d7244288759cffbae95c2acd441ef9451ad2
Jts76 <jts76@me.com>
2019-06-09 17:01:03 -0600

    Comments added.

--- BottlesOfBeer.java
+++ BottlesOfBeer.java
@@ -13,13 +13,15 @@ import acm.program.ConsoleProgram;
 
 public class BottlesOfBeer extends ConsoleProgram {
     public void run(){
+        // Get the number of bottles from the user.
         int bottles = readInt("How many bottles would you like to use?");
         println("");
+        // As long as there are bottles, print the poem.
         while (bottles > 0) {
                 println(bottles + " bottles of beer on the wall.");
                 println(bottles + " bottles of beer.");
                 println("You take one down, pass it around.");
-                bottles--;
+                bottles--; // The decrementor is placed here to remove a bottle after one is taken down.
                 println(bottles + " bottles of beer on the wall.");
                 println("");
         }
