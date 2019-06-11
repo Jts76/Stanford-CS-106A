ffe9786c8c64b535c3e7bfd9169a6594f3afdbfb
Jts76 <jts76@me.com>
2019-06-11 07:18:01 -0600

    Complete

--- Pyramid.java
+++ Pyramid.java
@@ -17,17 +17,25 @@ public class Pyramid extends GraphicsProgram{
     public void run(){
         int startWidth = getWidth() / 2 - (BRICK_WIDTH * (BRICKS_IN_BASE / 2));
         int startHeight = getHeight() / 2 + (BRICK_HEIGHT * (BRICKS_IN_BASE / 2));
-        for (int i = 0; i < BRICKS_IN_BASE; i--) {
-            makeBrick(startWidth, startHeight);
+        int numBricks = BRICKS_IN_BASE;
+        int brickX = startWidth;
+        for (int row = 0; row < BRICKS_IN_BASE; row++){
+
+            for (int brick = 0; brick < numBricks; brick++) {
+                makeBrick(brickX, startHeight);
+                brickX += BRICK_WIDTH;
+            }
+            startHeight -= BRICK_HEIGHT;
+            startWidth += BRICK_WIDTH / 2;
+            brickX = startWidth;
+            numBricks--;
+
         }
     }
     private void makeBrick(int cx, int cy){
         GRect brick = new GRect(cx, cy, BRICK_WIDTH, BRICK_HEIGHT);
         add (brick);
     }
-
-    /* Class Variables
-     * ---------------------------------------- */
 
 
     /* Constants
