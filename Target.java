67e9d24be0931a6a968031747626950d202d3644
Jts76 <jts76@me.com>
2019-06-11 22:38:47 -0600

    Updated

--- Target.java
+++ Target.java
@@ -5,28 +5,33 @@
  * alternates red and white fill colors with the
  * ring and inner bullseye being red
  */
- 
- import acm.graphics.*;
- import acm.program.*;
 
- import java.awt.*;
+import acm.graphics.*;
+import acm.program.*;
 
-public class Target extends GraphicsProgram{
- 
-     public void run() {
-         int centerWidth = getWidth() / 2;
-         int centerHeight = getHeight() / 2;
-         drawCircle(centerWidth - TARGET_WIDTH / 2,centerHeight - TARGET_HEIGHT / 2,TARGET_WIDTH,TARGET_HEIGHT, Color.RED);
-         drawCircle(centerWidth - (TARGET_WIDTH *.65) / 2,centerHeight - (TARGET_HEIGHT * .65) / 2,TARGET_WIDTH * .65,TARGET_HEIGHT * .65, Color.WHITE);
-         drawCircle(centerWidth - (TARGET_WIDTH *.3) / 2,centerHeight - (TARGET_HEIGHT * .3) / 2,TARGET_WIDTH * .3,TARGET_HEIGHT * .3, Color.RED);
-     }
-     private void drawCircle(double cx, double cy, double cWidth, double cHeight, Color color) {
-         GOval circle = new GOval(cx, cy, cWidth, cHeight);
-         circle.setFilled(true);
-         circle.setFillColor(color);
-         add(circle);
-     }
-     private static final int TARGET_WIDTH = 90;
-     private static final int TARGET_HEIGHT = 90;
+import java.awt.*;
 
- }
+public class Target extends GraphicsProgram {
+
+    public void run() {
+        int centerWidth = getWidth() / 2;
+        int centerHeight = getHeight() / 2;
+        drawCircle(centerWidth - TARGET_WIDTH / 2, centerHeight - TARGET_HEIGHT / 2,
+                TARGET_WIDTH, TARGET_HEIGHT, Color.RED);
+        drawCircle(centerWidth - (TARGET_WIDTH * .65) / 2, centerHeight - (TARGET_HEIGHT * .65) / 2,
+                TARGET_WIDTH * .65, TARGET_HEIGHT * .65, Color.WHITE);
+        drawCircle(centerWidth - (TARGET_WIDTH * .3) / 2, centerHeight - (TARGET_HEIGHT * .3) / 2,
+                TARGET_WIDTH * .3, TARGET_HEIGHT * .3, Color.RED);
+    }
+
+    private void drawCircle(double cx, double cy, double cWidth, double cHeight, Color color) {
+        GOval circle = new GOval(cx, cy, cWidth, cHeight);
+        circle.setFilled(true);
+        circle.setFillColor(color);
+        add(circle);
+    }
+
+    private static final int TARGET_WIDTH = 90;
+    private static final int TARGET_HEIGHT = 90;
+
+}
