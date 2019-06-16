2dd218fd63b8955c0df755992de653d8a775d583
Jts76 <jts76@me.com>
2019-06-16 10:17:02 -0600

    Added Commenting

--- Checkerboard.java
+++ Checkerboard.java
@@ -21,16 +21,24 @@ import java.awt.*;
 public class Checkerboard extends GraphicsProgram {
 
     public void run() {
+        /* The square size is the height of the windows divided
+         * by the number of rows wanted. */
         double sqSize = (double) getHeight() / N_ROWS;
         for (int i = 0; i < N_ROWS; i++) {
             for (int j = 0; j < N_COLUMNS; j++){
                 double x = j * sqSize - (N_ROWS / 2.0) * sqSize + (double) getWidth() / 2;
                 double y = i * sqSize;
                 GRect sq = new GRect(x, y, sqSize, sqSize);
+                /* Fill the Odd squares */
                 sq.setFilled((i + j) % 2 != 0);
                 sq.setFillColor(Color.GRAY);
                 add(sq);
+                /* Create the circle checker pieces, their size is 80% of the board squares
+                 * and they have a 10% cushion */
                 GOval oval = new GOval(x + sqSize * .1, y + sqSize * .1, sqSize *.8, sqSize *.8);
+
+                /* Fill the top 3 rows red and the bottom rows black.  Technically if you were to
+                 * add more pieces in the middle anything but the first 3 rows would be black. */
                 oval.setFilled(true);
                 if (i < 3) {
                     oval.setFillColor(Color.RED);
@@ -38,6 +46,7 @@ public class Checkerboard extends GraphicsProgram {
                     oval.setFillColor(Color.BLACK);
 
                 }
+                /* add the pieces to the first 3 rows and the last 3 rows. */
                 if ((i + j) % 2 != 0 && i < 3 || (i + j) % 2 != 0 && i > 4) {
                     add(oval);
                 }
@@ -46,7 +55,7 @@ public class Checkerboard extends GraphicsProgram {
     }
 
 /*
- *  CONSTANTS
+ *  Private CONSTANTS
  *  ================================== */
     private static final int N_ROWS = 8;
     private static final int N_COLUMNS = 8;
